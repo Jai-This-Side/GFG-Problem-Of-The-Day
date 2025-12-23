d6073f50e8b872226b284e6d462539fc06b651be
Jai-This-Side <jaisinghprsnl99@gmail.com>
2025-12-24 01:10:48 +0530

    Merge Overlapping Intervals (Optimal way)

--- mergeOverlappingIntervals.java
+++ mergeOverlappingIntervals.java
@@ -13,15 +13,15 @@ class mergeOverlappingIntervals {
 
         for (int i = 0; i < n; i++) {
             if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1)[1]) {
-                ans.add(arr[i]); 
-            }else {
+                ans.add(arr[i]);
+            } else {
                 ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], arr[i][1]);
             }
         }
         return ans;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int[][] arr = {
             {1, 3},
@@ -32,7 +32,7 @@ class mergeOverlappingIntervals {
 
         ArrayList<int[]> result = mergeOverlappingIntervals.mergeOverlap(arr);
 
-        for (int[] interval : result){
+        for (int[] interval : result) {
             System.out.println(interval[0] + " " + interval[1]);
         }
 
